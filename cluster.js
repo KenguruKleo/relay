5fb9de129013843514208b3510c4aa0331791642
kyemelianov <kyemelianov@lohika.com>
2018-04-16 17:11:57 +0300

    add cluster and workers

---
+++ cluster.js
@@ -0,0 +1,48 @@
+import cluster from 'cluster';
+import os from 'os';
+import server from './worker';
+
+const workers = {};
+let requests = 0;
+
+const numCPU = os.cpus().length;
+
+/** addWorker */
+const addWorker = ({ cluster, workers }) => {
+  const worker = cluster.fork();
+  const pid = worker.process.pid;
+  worker.on('message', message => {
+    console.log(`message: ${JSON.stringify(message)}`);
+    if (message.cmd === 'incrementRequestTotal') {
+      requests++;
+      Object.keys(workers)
+        .forEach(pid =>  {
+          const worker = workers[pid];
+          worker.send({
+            cmd: 'updateOfRequestTotal',
+            requests,
+          });
+        });
+    }
+  });
+  return { pid, worker };
+};
+
+if (cluster.isMaster) {
+  console.log(`Start: ${process.pid}, isMaster: ${cluster.isMaster}`);
+  for (let i = 0; i < numCPU; i++) {
+    const { pid, worker } = addWorker({ cluster, workers });
+    workers[pid] = worker;
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    const pid = worker.process.pid;
+    delete workers[pid];
+    console.log('Worker %s died.', pid);
+
+    const { pid: newPid, worker: newWorker } = addWorker({ cluster, workers });
+    workers[newPid] = newWorker;
+    console.log('Restart new worker with pid: %s', newPid);
+  });
+} else {
+  server();
+}
--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-import express from 'express';
-import graphQLHTTP from 'express-graphql';
-import path from 'path';
-import {schema} from './data/schema';
+import worker from './worker';
 
-const APP_PORT = 5000;
-
-const app = express();
-
-app.use('/', express.static(path.resolve(__dirname, 'public')));
-
-app.use('/graphql', graphQLHTTP({
-  schema: schema,
-  pretty: true,
-  graphiql: true
-}));
-
-app.listen(APP_PORT, () => {
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
-});
+worker();
